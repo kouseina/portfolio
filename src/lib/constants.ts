aa6704c09c3123086030ce314d7552a7ce73aaa7
Daffa Putera Kouseina <kouseinadaffa@gmail.com>
2024-09-12 13:00:52 +0700

    feat(constant): upwork

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,4 +3,5 @@ export const links = {
   email: {text: 'E-mail', url: 'kouseinadaffa@gmail.com'},
   telegram: {text: 'Telegram', url: 'https://t.me/kouseina'},
   inst: {text: 'Instagram', url: 'https://instagram.com/kouseina12'},
+  upwork: {text: 'Upwork', url: 'https://www.upwork.com/freelancers/~01a6b8ee098f913ae0'},
 }
