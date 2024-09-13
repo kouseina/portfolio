b210bc18c8a2476f096c63580b3eb1b0cd3bede8
Daffa Putera Kouseina <kouseinadaffa@gmail.com>
2024-09-13 14:46:26 +0700

    feat(works n link): add more

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,8 @@
 export const links = {
-  github: {text: 'GitHub', url: 'https://github.com/kouseina'},
-  email: {text: 'E-mail', url: 'kouseinadaffa@gmail.com'},
-  telegram: {text: 'Telegram', url: 'https://t.me/kouseina'},
-  inst: {text: 'Instagram', url: 'https://instagram.com/kouseina12'},
-  upwork: {text: 'Upwork', url: 'https://www.upwork.com/freelancers/~01a6b8ee098f913ae0'},
+  github: { text: 'GitHub', url: 'https://github.com/kouseina' },
+  email: { text: 'E-mail', url: 'https://mail.google.com/mail/?view=cm&fs=1&to=kouseinadaffa@gmail.com&su=Message+From+Web+Profile+(kouseina.vercel.app)' },
+  telegram: { text: 'Telegram', url: 'https://t.me/kouseina' },
+  // inst: {text: 'Instagram', url: 'https://instagram.com/kouseina12'},
+  upwork: { text: 'Upwork', url: 'https://www.upwork.com/freelancers/~01a6b8ee098f913ae0' },
+  linkedin: { text: 'Linkedin', url: 'https://www.linkedin.com/in/kouseina' },
 }
